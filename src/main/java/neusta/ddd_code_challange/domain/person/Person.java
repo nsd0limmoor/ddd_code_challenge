307384037c03cc89b6c05ecf5a68e4407100bee3
Nils Koschmieder <n.koschmieder@neusta.de>
2025-11-20 15:45:24 +0100

    Add `Person` domain model with validation and getter methods

---
+++ src/main/java/neusta/ddd_code_challange/domain/person/Person.java
@@ -0,0 +1,71 @@
+package neusta.ddd_code_challange.domain.person;
+
+import lombok.Getter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+public class Person {
+
+    private final Id id;
+    private final Name name;
+    private final String benutzerName;
+
+    public Person(String vorname, String nachname, String benutzerName) {
+        this.id = new Id(java.util.UUID.randomUUID());
+        this.name = new Name(vorname, nachname);
+        this.benutzerName = benutzerName;
+    }
+
+    public Person(String vorname, String nachname, String namensZusatz, String benutzerName) {
+        this.id = new Id(java.util.UUID.randomUUID());
+        this.name = new Name(vorname, nachname, namensZusatz);
+        this.benutzerName = benutzerName;
+    }
+
+    private record Id(UUID id) {
+    }
+
+    @Getter
+    private static final class Name {
+        private final String vorname;
+        private final String nachname;
+        private final String namensZusatz;
+        private final List<String> erlaubteNamensZusaetze = List.of("von", "van", "de");
+
+        private Name(String vorname, String nachname, String namensZusatz) {
+            if (vorname.isBlank()) {
+                throw new IllegalArgumentException("Vorname must not be empty");
+            }
+            if (nachname.isBlank()) {
+                throw new IllegalArgumentException("Nachname must not be empty");
+            }
+            if (vorname.length() > 100) {
+                throw new IllegalArgumentException("Vorname must not be longer than 100 characters");
+            }
+            if (nachname.length() > 100) {
+                throw new IllegalArgumentException("Nachname must not be longer than 100 characters");
+            }
+
+            if (namensZusatz != null && !erlaubteNamensZusaetze.contains(namensZusatz)) {
+                throw new IllegalArgumentException("Namenszusatz must be one of " + erlaubteNamensZusaetze);
+            }
+
+            this.vorname = vorname;
+            this.nachname = nachname;
+            this.namensZusatz = namensZusatz;
+        }
+
+        public Name(String vorname, String nachname) {
+            this(vorname, nachname, null);
+        }
+    }
+
+    private record BenutzerName(String benutzerName) {
+    }
+
+    public String getName() {
+        return name.vorname + (name.namensZusatz != null ? " " + name.namensZusatz : " ") + name.nachname + " (" + benutzerName + ")";
+    }
+}
