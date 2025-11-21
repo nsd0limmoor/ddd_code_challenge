010feab0baf4c161fc58e6092ecc43ee2cb060a6
Nils Koschmieder <n.koschmieder@neusta.de>
2025-11-21 09:52:52 +0100

    Add `existiertBelegungFuerPerson` method to `RaumRepository` and validation for unique room assignments

--- src/main/java/neusta/ddd_code_challange/domain/raum/RaumRepository.java
+++ src/main/java/neusta/ddd_code_challange/domain/raum/RaumRepository.java
@@ -1,13 +1,16 @@
 package neusta.ddd_code_challange.domain.raum;
 
+import neusta.ddd_code_challange.domain.person.Person;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RaumRepository {
 
-  Raum ladeRaum(Raum.Nummer nummer);
+    Raum ladeRaum(Raum.Nummer nummer);
 
-  Raum legeRaumAn(Raum raum);
+    Raum legeRaumAn(Raum raum);
 
-  Raum aktualisiereRaum(Raum raum);
+    Raum aktualisiereRaum(Raum raum);
+
+    boolean existiertBelegungFuerPerson(Person.Id personId);
 }
--- src/main/java/neusta/ddd_code_challange/infrastructure/raum/repository/RaumRepositoryImpl.java
+++ src/main/java/neusta/ddd_code_challange/infrastructure/raum/repository/RaumRepositoryImpl.java
@@ -2,6 +2,8 @@ package neusta.ddd_code_challange.infrastructure.raum.repository;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import neusta.ddd_code_challange.domain.person.Person;
 import neusta.ddd_code_challange.domain.raum.Raum;
 import neusta.ddd_code_challange.domain.raum.RaumRepository;
 import org.springframework.stereotype.Repository;
@@ -9,20 +11,26 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class RaumRepositoryImpl implements RaumRepository {
 
-  private Map<Raum.Nummer, Raum> raeume = new HashMap<>();
+    private final Map<Raum.Nummer, Raum> raeume = new HashMap<>();
 
-  @Override
-  public Raum ladeRaum(final Raum.Nummer nummer) {
-    return raeume.get(nummer);
-  }
+    @Override
+    public Raum ladeRaum(final Raum.Nummer nummer) {
+        return raeume.get(nummer);
+    }
 
-  @Override
-  public Raum legeRaumAn(final Raum raum) {
-    return raeume.putIfAbsent(raum.getNummer(), raum);
-  }
+    @Override
+    public Raum legeRaumAn(final Raum raum) {
+        return raeume.putIfAbsent(raum.getNummer(), raum);
+    }
 
-  @Override
-  public Raum aktualisiereRaum(final Raum raum) {
-    return raeume.put(raum.getNummer(), raum);
-  }
+    @Override
+    public Raum aktualisiereRaum(final Raum raum) {
+        return raeume.put(raum.getNummer(), raum);
+    }
+
+    @Override
+    public boolean existiertBelegungFuerPerson(Person.Id personId) {
+        return raeume.values().stream()
+            .anyMatch(raum -> raum.getPersonen().contains(personId));
+    }
 }
